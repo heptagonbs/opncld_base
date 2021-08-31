549bbc81b1546f8e1b09d01396680fe7126724e6
Andrei Condurachi <andrei@condurachi.ro>
2021-08-31 13:25:54 +0300

    packages & composer updates

--- opncld-add-user.sh
+++ opncld-add-user.sh
@@ -2,6 +2,6 @@
 
 OPNCLD_EXISTS=$(getent passwd opncld)
 if [ -z $OPNCLD_EXISTS ]; then
-        useradd -mU -d /home/opncld opncld
+        useradd -mU -s bash -d /home/opncld opncld
         usermod -aG docker opncld
 fi
--- opncld-init.sh
+++ opncld-init.sh
@@ -6,13 +6,18 @@ echo y | apt upgrade
 ### END OF Ubuntu updates
 
 ### Install Docker & Docker Compose
-# Update the apt package index and install packages to allow apt to use a repository over HTTPS
 echo y | apt install \
-        apt-transport-https \
-        ca-certificates \
-        curl \
-        gnupg \
-        lsb-release
+        # Update the apt package index and install packages to allow apt to use a repository over HTTPS
+        apt-transport-https ca-certificates curl gnupg lsb-release \
+        # Required packages
+        zip php php-xml php-gd
+
+# Composer
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('sha384', 'composer-setup.php') === '756890a4488ce9024fc62c56153228907f1545c228516cbf63f885e036d37e9a59d27d63f46af1d4d07ee0f76181c7d3') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+mv composer.phar /usr/bin/composer
 
 # Add Docker’s official GPG key
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --yes --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
